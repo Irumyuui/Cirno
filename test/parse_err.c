2937e631c6464669fa94c2af1432c56a52ed579f
ChirunoC9 <ciel_katana@outlook.com>
2024-01-09 16:40:43 +0800

    提交测试代码

---
+++ test/parse_err.c
@@ -0,0 +1,13 @@
+int Foo()
+{
+    int k = 1;
+    if ()
+    {
+        return 1;
+    }
+}
+
+int main()
+{
+    Foo(int);
+}
---
+++ test/unkown_token.c
@@ -0,0 +1,9 @@
+fn foo(x : i32) -> i32
+{
+    x
+}
+
+fn main()
+{
+    println!("{}", foo());    
+}
